092b13ed1a6ed3bc2f3494d68e3a7cb6da63cf60
sarumiishi <43409960+pierreSALMI@users.noreply.github.com>
2020-04-13 19:51:00 +0200

    Revert "Revert "a""
    This reverts commit 54920f7234312da6f951e3ff488ad5de9da19a57.

--- LaReponseD/resources/views/profileBlade/show.blade.php
+++ LaReponseD/resources/views/profileBlade/show.blade.php
@@ -16,66 +16,45 @@
             {{ session('status') }}
         </div>
     @endif
-    </div>
-    <div class="card" style="width:60vw;">
-        <div class="card-header">
-            <h2 class="float-left">Profile de : {{ $profile->pseudo }}</h2>
-            @if ($profile->profileId == Auth::id())
-                <form action="{{ route('profile.edit', $profile->profileId ) }}" method="GET">
-                    {{ method_field('GET') }}
-                    {{ csrf_field() }}
-                    <button type="submit" class="btn btn-primary float-right">Modifier</button>
-                </form>
-            @endif
-        </div>
-        <div class="card-body row">
-            <div style="float: left; width: 30%; text-align:center;">
-                <img class="img-responsive img-rounded" style="width:280px; height:280px;" src="/images/avatar/{{ $profile->avatar }}" alt="User picture">
-                <h2 style="margin:10px;">{{ $profile->pseudo }}</h2>
-                <p>Membre depuis : <strong>{{ $profile->userSince() }}</strong></p>
+        <div class="card">
+            <div class="card-header">
+                <h2 class="float-left">Profile de : {{ $profile->pseudo }}</h2>
+                @if ($profile->profileId == Auth::id())
+                    <form action="{{ route('profile.edit', $profile->profileId ) }}" method="GET">
+                        {{ method_field('GET') }}
+                        {{ csrf_field() }}
+                        <button type="submit" class="btn btn-primary float-right">Modifier</button>
+                    </form>
+                @endif
             </div>
-            <div style="float: right; width: 70%; padding:10px;">
-                <table class="table table-striped" style="display:table; width:100%;">
-                    <thead>
-                        <tr>
-                            <td style="width:45%;">Titre</td>
-                            <td style="width:20%;">Joué</td>
-                            <td style="width:20%;">Catégorie</td>
-                            <td style="width:15%;"></td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    @foreach ($profile->user->quiz as $quiz)
-                        <tr>
-                            <td style="width:45%;"><h4>{{ $quiz->titre }}</h4></td>
-                            <td style="width:20%;">
-                                <?php
-                                switch ($quiz->compteur) {
-                                    case NULL:
-                                    case 0:
-                                        echo "Pas encore joué";
-                                        break;
-                
-                                    default:
-                                        echo "Joué ".$quiz->compteur." fois.";
-                                        break;
-                                }
-                                ?>
-                            </td>
-                            <td style="width:20%;">
-                                <a href="/quiz/categorie/{{$quiz->category->categoryId}}" class="btn btn-primary btn-block"> {{$quiz->category->categoryName}}</a>
-                            </td>
-                            <td style="width:15%;">
-                                <form action="{{ route('quiz.show', $quiz->quizId ) }}" method="GET">
-                                    {{ method_field('GET') }}
-                                    {{ csrf_field() }}
-                                    <button type="submit" class="btn btn-primary"><i class="fas fa-play"></i></button>
-                                </form>
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
+            <div class="card-body">
+                <img class="img-responsive img-rounded" style="width:200px; height:200px;" src="/images/avatar/{{ $profile->avatar }}" alt="User picture">
+                <h2>{{ $profile->pseudo }}</h2>
+                <p>Membre depuis : <strong>{{ $profile->userSince() }}</strong></p>
+                <div class="mainFlex">
+                @foreach ($profile->user->quiz as $quiz)
+                    <div class="border border-secondary" style="padding:3px;">
+                        <h4>{{ $quiz->titre }}</h4>
+                        <?php
+                        switch ($quiz->compteur) {
+                            case NULL:
+                            case 0:
+                                echo "Pas encore joué";
+                                break;
+        
+                            default:
+                                echo "Joué ".$quiz->compteur." fois.";
+                                break;
+                        }
+                        ?>
+                        <form action="{{ route('quiz.show', $quiz->quizId ) }}" method="GET">
+                            {{ method_field('GET') }}
+                            {{ csrf_field() }}
+                            <button type="submit" class="btn btn-primary btn-block"><i class="fas fa-play"></i></button>
+                        </form>
+                    </div>
+                @endforeach
+                </div>
             </div>
         </div>
     </div>
