9bf19e3eed828bcf35ebc66f3d6b62418c638fab
Soteded <43409872+Soteded@users.noreply.github.com>
2020-04-13 12:03:44 +0200

    Front + Dashboard
    Dashboard reports + starting front

--- LaReponseD/public/js/showQuiz.js
+++ LaReponseD/public/js/showQuiz.js
@@ -149,7 +149,7 @@ window.addEventListener("hashchange", selectPage);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! Y:\B2\LaReponseD-v2\LaReponseD\resources\js\showQuiz.js */"./resources/js/showQuiz.js");
+module.exports = __webpack_require__(/*! D:\Workspace\B2\ProjetWeb-LaReponseD_-_v2\LaReponseD\resources\js\showQuiz.js */"./resources/js/showQuiz.js");
 
 
 /***/ })
--- LaReponseD/resources/views/categoryBlade/indexU.blade.php
+++ LaReponseD/resources/views/categoryBlade/indexU.blade.php
@@ -14,16 +14,16 @@
         </div><br />
     @endif
     </div>
-    <div class="card" style="width: 50%; display: block; margin-left: auto; margin-right: auto;">
+    <div class="card" style="width:100%; margin-left: auto; margin-right: auto;">
         <div class="card-header">
-            Toutes les catégories
+            <h2>Toutes les catégories</h2>
         </div>
-        <div class="card-body">
+        <div class="card-body mainFlex categIndex">
             @foreach ($categories as $category)
-                <a href="{{ route('category.show', $category->categoryId) }}">
-                    <div>
-                    <h4>{{ $category->categoryName }}</h4>
-                    Quizs : {{ count($category->quizs) }}
+                <a href="{{ route('category.show', $category->categoryId) }}" class="card col-lg-2 col-md-4 mb-2">
+                    <div class="card-body">
+                        <h4>{{ $category->categoryName }}</h4>
+                        Quizs : {{ count($category->quizs) }}
                     </div>
                 </a>
             @endforeach
